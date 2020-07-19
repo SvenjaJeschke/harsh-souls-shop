4b1d8ae8479376adf5f04c935661736dd5ac4c18
SvenjaJeschke <svenja.jeschke12@gmail.com>
2020-07-19 20:55:36 +0200

    webpack bug solved

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
-mix.webpackConfig({
+/*mix.webpackConfig({
     resolve: {
         rules: [
             {
@@ -43,4 +43,4 @@ mix.webpackConfig({
             }
         ]
     }
-});
+});*/
