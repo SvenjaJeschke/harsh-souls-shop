a25fd34a918376c8dc85dd395e7d3777e18d4f43
Chinyoka1 <svenja.jeschke12@gmail.com>
2020-10-03 16:06:37 +0200

    added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <title>{{ env('APP_NAME') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <link rel="icon" href="{{ URL::asset('/assets/harshsouls.png') }}" type="image/x-icon"/>
 </head>
 <body>
 <div id="app">
