ad127110dfe0400700e648004d8cb837e46d596d
Chinyoka1 <svenja.jeschke12@gmail.com>
2020-10-03 21:51:18 +0200

    added custom maintenance mode page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+        background-color: #333333;
+        font: 20px Helvetica, sans-serif;
+        color: #f8fafc;
+    }
+
+    article {
+        display: block;
+        text-align: center;
+        width: 650px;
+        margin: 0 auto;
+    }
+</style>
+
+<article>
+    <p>Sorry, Harsh Souls Shop is not available at the moment. <br><br>It should be back again in a few minutes. Otherwise, please contact the developers on discord!</p>
+</article>
